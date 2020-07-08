04b0b0058795de01d0a90a21c7568312bb1bcd32
lileichuang <leichuang.li@ifchange.com>
2020-07-08 18:09:22 +0800

    zuul

--- zuul_test/src/main/java/cn/test/zuul/api/Filter.java
+++ zuul_test/src/main/java/cn/test/zuul/api/Filter.java
@@ -1,8 +1,45 @@
 package cn.test.zuul.api;
 
-public class Filter {
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class Filter extends ZuulFilter {
 
 
+    @Override
+    public String filterType() {
+        return "pre";
+    }
 
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
 
+    @Override
+    public boolean shouldFilter() {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String token = request.getHeader("token");
+        if (token == null) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Object run()   {
+        try {
+            RequestContext ctx = RequestContext.getCurrentContext();
+            HttpServletResponse response =ctx.getResponse();
+            response.getWriter().write("token 不可为空");
+        }catch (Exception e){
+        }
+        return  null;
+    }
 }
